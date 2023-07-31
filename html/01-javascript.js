33ddb538ce00b0a75caccc9ee40e9ed646c0430b
emin saridogan <eminsaridogan04@gmail.com>
2023-07-31 11:43:35 +0300

    javascript dom ve islemleri

---
+++ html/01-javascript.js
@@ -0,0 +1,27 @@
+let isim = "emin"
+console.log(isim)
+
+// let userName ="Emin"
+// let password ="1212121"
+// const SERVER_PASSWORD = "22223456762345687"
+// let pasword + userName = userPass  
+// console.log
+
+
+// let ad= `Aycan`;
+// let soyad= `Yerdelen`; 
+// console.log(`benim adim ${ad} ${soyad}`) 
+
+// const Ad = `Aycan`
+// const Soyad = `Yerdelen`
+// const yaş = `21 yaşinda`
+// const göz_rengi = `göz rengi Yeşil`
+
+// console.log (Ad ,Soyad ,yaş , göz_rengi)
+
+
+// let url = "www.kodluyoruz.org" 
+// console.log(url)
+
+let title = document.getElementsByTagName ('h2')
+console.log(title)
