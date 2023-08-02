8f8446834c586ca2b8c44955d49f1a1006d7848f
emin saridogan <eminsaridogan04@gmail.com>
2023-08-02 12:15:18 +0300

    function started

--- html/02-javascript.js
+++ html/02-javascript.js
@@ -1,9 +1,16 @@
-let Name = prompt ("kullanıcı adınız :")
-let age = prompt ("yasınızı girin")
- if (Name && age  >= 18){
-    console.log("ehliyet alabilirsiniz")
- } else if (!Name) {
-    console.log("kullanıcı adınız yok")
- } else if (!(age >= 18)) {
-    console.log("yasınızı girin veya yasnız 18den kücük" )
+// let Name = prompt ("kullanıcı adınız :")
+// let age = prompt ("yasınızı girin")
+//  if (Name && age  >= 18){
+//     console.log("ehliyet alabilirsiniz")
+//  } else if (!Name) {
+//     console.log("kullanıcı adınız yok")
+//  } else if (!(age >= 18)) {
+//     console.log("yasınızı girin veya yasnız 18den kücük" )
+//  }
+
+
+ function hello() {
+    console.log("MERHABA")
  }
+  
+ hello()
