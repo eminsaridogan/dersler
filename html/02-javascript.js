842b2a38c799969c42b4da7eb3e060b596f1ec0c
emin saridogan <eminsaridogan04@gmail.com>
2023-08-03 12:17:02 +0300

    function egzersizleri

--- html/02-javascript.js
+++ html/02-javascript.js
@@ -8,9 +8,63 @@
 //     console.log("yasınızı girin veya yasnız 18den kücük" )
 //  }
 
+// let firstName = prompt ("kullanıcı adınız")
+// console.log(firstName)
 
- function hello() {
-    console.log("MERHABA")
- }
+                                                    // bu kullanım yalnıs
+//  function hello() {
+//     console.log(`MERHABA ${firstName}`)
+//  }
   
- hello()
+//  hello()
+
+
+ function hello(firstName="") {
+    console.log(`Merhaba ${firstName}`)
+ }
+                                                    // bu kullanım dogru
+ hello("Apple")
+
+
+
+
+ function hosgeldin()
+{
+    return "Sitemize Hoş Geldiniz";
+}
+
+console.log(hosgeldin());
+
+
+
+// function hosgeldin()
+// {
+//     console.log ("Sitemize Hoş Geldiniz");
+// }
+
+// hosgeldin()
+
+// let greeting = document.querySelector("#greeting")
+// greeting.addEventListener("click",domClick)
+
+// function domClick(  ) {
+//     console.log("Tiklandi")
+//     this.style.color = "purple"
+//     console.log(this.innerHTML = "Tiklandigi için bigi degisti")
+// }
+
+let counter = 0 ;
+let counterDOM = document.querySelector ("#counter")
+let incraeseDOM = document.querySelector ("#incraese")
+let decraeseDOM = document.querySelector ("#decraese")
+
+counterDOM.innetHTML = counter
+
+incraeseDOM.addEventListener("click",clickEvent)
+decraeseDOM.addEventListener("click",clickEvent)
+
+function clickEvent() {
+    console.log(this.id)
+    this.id == "incraese" ? counter += 1 : counter -= -1 
+    counterDOM.innerHTML = counter
+}
