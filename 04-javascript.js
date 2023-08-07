ae61cafbb52fa0ca7260f4e02695414844cf0ddc
emin saridogan <eminsaridogan04@gmail.com>
2023-08-07 12:08:42 +0300

    Arraylar (Dizi)

---
+++ 04-javascript.js
@@ -0,0 +1,32 @@
+let domain = "kodluyoruz"
+let isActive = false 
+let items = [12, 23, 36, isActive, domain]
+console.log (items)
+
+let emptyArray = []
+
+console.log (
+    items.length
+)
+
+// document.querySelector("#info").inner.HTML = items.length
+
+console.log(items[0] )
+
+console.log(items[1])
+
+console.log(items[3-2])
+
+console.log(items[4-1])
+
+console.log(items[5-1])
+
+
+
+console.log(
+    Array.isArray (items)
+)
+
+console.log(
+    Array.isArray ( [] )
+)
