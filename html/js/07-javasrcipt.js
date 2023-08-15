359dc8260f300939b6d99cab49b96c507494474c
emin saridogan <eminsaridogan04@gmail.com>
2023-08-15 12:02:18 +0300

    break and continue

---
+++ html/js/07-javasrcipt.js
@@ -0,0 +1,17 @@
+const LOREM_LİST = [
+  "lorem" , "ipsum", "dolor", " amet" ,"consectetur, " 
+]    
+
+let counter = 0 
+
+// for (; counter < 10; counter++   ) {
+//     console.log(counter)
+//     if (counter === 3) {break}
+//     console.log(counter)
+// }
+
+// for (; counter < 10; counter++   ) {
+//     console.log(counter)
+//     if (counter === 3) {continue}
+//     console.log(counter)
+// }
